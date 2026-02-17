3f027bc032230cc702129eb0ed58f1d82b2a0917
Manuel Odendahl <wesen@ruinwesen.com>
2026-02-17 14:17:00 -0500

    refactor(engine): remove unused createDSLApp scaffold API

--- packages/engine/src/app/index.ts
+++ packages/engine/src/app/index.ts
@@ -1,3 +1,2 @@
 export { createAppStore, type CreateAppStoreOptions } from './createAppStore';
-export { createDSLApp, type DSLAppConfig } from './createDSLApp';
 export { type CardStoriesConfig, createStoryHelpers, toStoryParam } from './generateCardStories';
